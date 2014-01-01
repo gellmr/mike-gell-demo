1952125d360e35455eb313fc781f18b50f36ba26
Mike Gell <gellmr@gmail.com>
2014-01-01 15:38:24 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fuzzybear::Application.config.secret_key_base = '4ad83a7545db973aad5b42537033b22e88b0a822d61ad9a9fcd7356b66bab0d85f707224c52a7f985d80ec4474b7ff5c3df5a863cd4df79cfeb726c7a53db555'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fuzzybear::Application.config.session_store :cookie_store, key: '_fuzzybear_session'
