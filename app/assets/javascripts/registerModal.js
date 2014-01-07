297781290fb887450b5cfa5c7ba140f15e538a8e
Mike Gell <gellmr@gmail.com>
2014-01-07 12:35:47 +0800

    Refactored registerModal.js

--- app/assets/javascripts/registerModal.js
+++ app/assets/javascripts/registerModal.js
@@ -1,3 +1,52 @@
+function showErrors(errors) {
+  $('#form-group-email').removeClass('alert alert-warning');
+  $('#form-group-password').removeClass('alert alert-warning');
+  $('#form-group-password-confirmation').removeClass('alert alert-warning');
+
+  function showWarningClass() {
+    $('#form-group-email').addClass('alert');
+    $('#form-group-password').addClass('alert');
+    $('#form-group-password-confirmation').addClass('alert');
+  };
+
+  var emailErrors = errors.responseJSON['email'];
+  var pwErrors = errors.responseJSON['password'];
+  var pwConfErrors = errors.responseJSON['password_confirmation'];
+
+  $('#form-group-email .error-message').empty();
+  $('#form-group-password .error-message').empty();
+  $('#form-group-password-confirmation .error-message').empty();
+
+  if (typeof(emailErrors) != "undefined" && emailErrors.length > 0) {
+    showWarningClass();
+    $('#form-group-email').addClass("alert-warning");
+    for(var propName in emailErrors) {
+      emailErrors[propName] = capitaliseFirstLetter(emailErrors[propName]);
+    };
+    $('#form-group-email .error-message').append(emailErrors.join(', '));
+  }
+
+  if (typeof(pwErrors) != "undefined" && pwErrors.length > 0) {
+    showWarningClass();
+    $('#form-group-password').addClass("alert-warning");
+    $('#form-group-password-confirmation').addClass("alert-warning");
+    for(var propName in pwErrors) {
+      pwErrors[propName] = capitaliseFirstLetter(pwErrors[propName]);
+    };
+    $('#form-group-password .error-message').append(pwErrors.join(', '));
+  }
+
+  if (typeof(pwConfErrors) != "undefined" && pwConfErrors.length > 0) {
+    showWarningClass();
+    $('#form-group-password').addClass("alert-warning");
+    $('#form-group-password-confirmation').addClass("alert-warning");
+    for(var propName in pwConfErrors) {
+      pwConfErrors[propName] = capitaliseFirstLetter(pwConfErrors[propName]);
+    };
+    $('#form-group-password-confirmation .error-message').append(pwConfErrors.join(', '));
+  }
+};
+
 $(document).ready(function() {
   $('#registerModalSubmit').click(function() {
 
@@ -27,61 +76,7 @@ $(document).ready(function() {
 
         // bad request
         400: function(errors) {
-          // console.log("failed. " + errors.responseText);
-
-          $('#form-group-email').removeClass('alert alert-warning');
-          $('#form-group-password').removeClass('alert alert-warning');
-          $('#form-group-password-confirmation').removeClass('alert alert-warning');
-
-          function showWarningClass() {
-            $('#form-group-email').addClass('alert');
-            $('#form-group-password').addClass('alert');
-            $('#form-group-password-confirmation').addClass('alert');
-          };
-
-          var emailErrors = errors.responseJSON['email'];
-          var pwErrors = errors.responseJSON['password'];
-          var pwConfErrors = errors.responseJSON['password_confirmation'];
-
-          $('#form-group-email .error-message').empty();
-          $('#form-group-password .error-message').empty();
-          $('#form-group-password-confirmation .error-message').empty();
-
-          if (typeof(emailErrors) != "undefined" && emailErrors.length > 0) {
-            showWarningClass();
-            $('#form-group-email').addClass("alert-warning");
-            for(var propName in emailErrors) {
-              emailErrors[propName] = capitaliseFirstLetter(emailErrors[propName]);
-            };
-            $('#form-group-email .error-message').append(emailErrors.join(', '));
-          }
-
-          if (typeof(pwErrors) != "undefined" && pwErrors.length > 0) {
-            showWarningClass();
-            $('#form-group-password').addClass("alert-warning");
-            $('#form-group-password-confirmation').addClass("alert-warning");
-            for(var propName in pwErrors) {
-              pwErrors[propName] = capitaliseFirstLetter(pwErrors[propName]);
-            };
-            $('#form-group-password .error-message').append(pwErrors.join(', '));
-          }
-
-          if (typeof(pwConfErrors) != "undefined" && pwConfErrors.length > 0) {
-            showWarningClass();
-            $('#form-group-password').addClass("alert-warning");
-            $('#form-group-password-confirmation').addClass("alert-warning");
-            for(var propName in pwConfErrors) {
-              pwConfErrors[propName] = capitaliseFirstLetter(pwConfErrors[propName]);
-            };
-            $('#form-group-password-confirmation .error-message').append(pwConfErrors.join(', '));
-          }
-
-          // $('#error_messages').empty();
-          // for(var propName in errors.responseJSON) {
-          //   var value = errors.responseJSON[propName];
-          //   console.log(propName + ': ' + value);
-          //   // $('#error_messages').append('<li>' + propName + ': ' + value + '</li>');
-          // }
+          showErrors(errors);
         }
       }
 
