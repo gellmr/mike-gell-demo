f35f735062d35d79f5ce3e7c5888a8b4fd531f0f
Mike Gell <gellmr@gmail.com>
2014-02-11 18:03:53 +0800

    Altered the navbar text responsiveness.

--- app/assets/javascripts/windowResize.js
+++ app/assets/javascripts/windowResize.js
@@ -1,21 +1,21 @@
 
 function normalMode() {
   // Normal navbar text size
-  $('.mg-navbar-title').css('font-size', '18px');
   $('.mg-navbar-link').css('font-size', '14px');
 }
 
 function smallMode() {
   // Shrink navbar text because window is small
-  $('.mg-navbar-title').css('font-size', '11px');
   $('.mg-navbar-link').css('font-size', '11px');
   $('.container').css('min-width', '222px');
 }
-
+var c = 0;
 $(document).ready(function() {
   
   // This makes the navbar text become tiny, if the window is very small.
   $(window).resize( function() {
+    console.log('resized' + c);
+    c = c + 1;
     var size = $('body nav.navbar').css('width');
     if (parseInt(size) > 382) {
       normalMode();
