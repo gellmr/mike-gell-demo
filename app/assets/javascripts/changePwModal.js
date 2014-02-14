42b68bc1f5ed40c4ef8199e03d08d189e07e6a01
Mike Gell <gellmr@gmail.com>
2014-02-14 14:51:15 +0800

    Removed *PwModal.js files.

--- app/assets/javascripts/changePwModal.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  
-  $('#changePwModalSubmit').click(function() {
-    
-    console.log("Change password modal");
-
-    $('#changePwModal').modal('hide');
-  });
- });
--- app/assets/javascripts/forgotPwModal.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  
-  $('#forgotPwModalSubmit').click(function() {
-    
-    console.log("Try to request new password");
-
-    $('#forgotPwModal').modal('hide');
-  });
- });
