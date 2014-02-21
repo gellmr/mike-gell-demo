c68a407ff1e499f1a218c6fb409cffa5ece6d8cb
Mike Gell <gellmr@gmail.com>
2014-02-21 15:44:39 +0800

    Cart values now persist on store page.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,14 +1,16 @@
 class StoreController < ApplicationController
   def index
     # Get all products in the store.
-    debug_print_cart()
-    
+    # debug_print_cart()
+
     @products = []
-    Product.all.each do |p|
-      productId = p.id
+    Product.all.each do |product|
+      productId = product.id.to_s
+      qty = user_cart[productId]
+      
       @products.push({
-        record: p,
-        cart_qty: user_cart[productId] || 0
+        record: product,
+        cart_qty: qty
       })
     end
     # Rails.logger.debug "@products: #{@products.to_yaml}"
