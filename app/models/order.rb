805ea8aad65626cd9b4eca9184ba97c06cb2996f
Mike Gell <gellmr@gmail.com>
2014-02-24 12:40:07 +0800

    Validation rules for order.rb and ordered_product.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,5 @@
 class Order < ActiveRecord::Base
+  validates :user, presence: true
   belongs_to :user # gives us user_id (FK)
   has_many :ordered_products, dependent: :destroy
   has_many :products, through: :ordered_products
--- app/models/ordered_product.rb
+++ app/models/ordered_product.rb
@@ -1,4 +1,5 @@
 class OrderedProduct < ActiveRecord::Base
+  validates :order, :product, presence: true
   belongs_to :order     # gives us order_id   (FK)
   belongs_to :product   # gives us product_id (FK)
 end
