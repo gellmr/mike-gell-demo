a06343c7807a6b86cebaa5d85f2889f35167c134
Mike Gell <gellmr@gmail.com>
2014-03-11 13:20:19 +0800

    Added email validation regex for password reset.

--- app/controllers/password_reset_controller.rb
+++ app/controllers/password_reset_controller.rb
@@ -6,9 +6,38 @@ class PasswordResetController < ApplicationController
   end
 
   def create
-    Rails.logger.debug 'User has submitted a form, to reset their password.'
-    Rails.logger.debug "email: #{params[:password_reset_email]}"
-    
-  end
+    email = sane_params[:email].downcase
+    Rails.logger.debug "email.downcase: #{email}"
 
+    # Informative logging
+    if email.blank?
+      Rails.logger.debug "(validation fail) Email is blank"
+    end
+    if email == "your email address"
+      Rails.logger.debug "(validation fail) Email is placeholder string"
+    end
+
+    if email.blank? || email == "your email address" || !valid_email?(email)
+      flash[:warning] =  'Please provide a valid email address.'
+      redirect_to new_password_reset_path
+    end
+  end
+  
+  private
+    # see http://www.regular-expressions.info/email.html
+    def valid_email?(email_string)
+      regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+      if ( email_string =~ regex ) != nil
+        Rails.logger.debug "(validation PASS) email regex)"
+        return true
+      else
+        Rails.logger.debug "(validation fail) email regex)"
+        return false
+      end
+    end
+
+    # Strong params
+    def sane_params
+      params.permit(:email)
+    end
 end
