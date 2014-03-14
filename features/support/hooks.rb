fc978b8628ff3f6ebb64dbdc280c07ee3ec4ce20
Mike Gell <gellmr@gmail.com>
2014-03-14 13:06:59 +0800

    Added empty before hook to features/support/hooks

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,6 +1,10 @@
+Before do
+  # Reset the database here.
+end
+
 After do |scenario|
   if scenario.failed?
-  	# write a snapshot of the html to disk for later inspection
+    # write a snapshot of the html to disk for later inspection
     save_page
   end
 end
