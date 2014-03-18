a6deb25399a52ad0e84f9c5b11e9bc996c6864d9
Mike Gell <gellmr@gmail.com>
2014-03-18 14:06:52 +0800

    Added MEDIA argument for selenium breakpoint testing.

--- features/support/capybara_poltergeist.rb
+++ features/support/capybara_poltergeist.rb
@@ -3,6 +3,11 @@ require 'capybara/cucumber'
 require 'capybara/poltergeist'
 require 'phantomjs'
 
+def set_selenium_window_size(width, height)
+  window = Capybara.current_session.driver.browser.manage.window
+  window.resize_to(width, height)
+end
+
 if ENV['BROWSER']
   # On demand: non-headless tests via Selenium/WebDriver
   # To run the scenarios in browser (default: Firefox), use the following command line:
@@ -10,6 +15,27 @@ if ENV['BROWSER']
   # or (to have a pause of 1 second between each step):
   # BROWSER=true PAUSE=1 bundle exec cucumber
   Capybara.default_driver = :selenium
+  safety_width = 10
+  height = 1024
+  case ENV['MEDIA']
+    when 'xs'
+      puts "Testing breakpoint XS"
+      set_selenium_window_size(480 - safety_width, height)
+    when 'sm'
+      puts "Testing breakpoint SM"
+      set_selenium_window_size(768 - safety_width, height)
+    when 'md'
+      puts "Testing breakpoint MD"
+      set_selenium_window_size(992 - safety_width, height)
+    when 'lg'
+      puts "Testing breakpoint LG"
+      set_selenium_window_size(1200 - safety_width, height)
+    else
+      puts "Testing breakpoint XL (this is the default)"
+      set_selenium_window_size(1280, height)
+  end
+  
+
   AfterStep do
     sleep (ENV['PAUSE'] || 0).to_i
   end
